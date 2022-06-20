0b787924b6cdb030da18a4c55d06981efdc7d889
eric-klimowich <eklimowich@hotmail.com>
2022-06-20 11:38:49 -0400

    Unit 3 Day 9

---
+++ 01-mern-shopping-cart-part-1/starter-code/sei-cafe-codealong/src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function NewOrderPage({ user, setUser }) {
+  const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  // Obtain a ref object
+  const categoriesRef = useRef([]);
+
+  // useEffect(function() {
+  //   console.log('NewOrderPage rendered');
+  // });
+  
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat]
+      }, []);
+      setActiveCat(categoriesRef.current[1]);
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);
+  // the empty dependency array above will result in 
+  // the function running after the FIRST render
+  // only
+
+  return (
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
+  );
+}
---
+++ 03-04-mern-stack-hackathon/completed-code/mern-hackathon/src/components/MenuList/MenuList.jsx
@@ -0,0 +1,17 @@
+import './MenuList.css';
+import MenuListItem from '../MenuListItem/MenuListItem';
+
+export default function MenuList({ menuItems, handleAddToOrder }) {
+  const items = menuItems.map(item =>
+    <MenuListItem
+      key={item._id}
+      handleAddToOrder={handleAddToOrder}
+      menuItem={item}
+    />
+  );
+  return (
+    <main className="MenuList">
+      {items}
+    </main>
+  );
+}
