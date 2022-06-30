0e1ea1c30609b26d47d774276e1f4d0995fbc1dd
kmcaloney15 <katymcaloney@gmail.com>
2022-06-30 11:29:12 -0400

    possibly finished - who actually knows

--- 01-mern-shopping-cart-part-1/starter-code/sei-cafe-codealong/models/order.js
+++ 01-mern-shopping-cart-part-1/starter-code/sei-cafe-codealong/models/order.js
@@ -1,48 +1,72 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 // Require the itemSchema below
-const itemSchema = require('./itemSchema');
+const itemSchema = require("./itemSchema");
 
 // line items are embeded in orders since you will always need a order to have line items
 
-const lineItemSchema = new Schema({
-    // Set qty to 1 when new item pushed into lineItems
-    qty: { type: Number, default: 1 },
-    item: itemSchema
-  }, {
-    timestamps: true,
-    // Add this to ensure virtuals are serialized
-  toJSON: { virtuals: true }
-  });
+const lineItemSchema = new Schema(
+    {
+        // Set qty to 1 when new item pushed into lineItems
+        qty: { type: Number, default: 1 },
+        item: itemSchema,
+    },
+    {
+        timestamps: true,
+        // Add this to ensure virtuals are serialized
+        toJSON: { virtuals: true },
+    }
+    );
+    
+    const orderSchema = new Schema(
+      {
+        // An order belongs to a user
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        // Embed an order's line items is logical
+        lineItems: [lineItemSchema],
+        // A user's unpaid order is their "cart"
+        isPaid: { type: Boolean, default: false },
+      },
+      {
+        timestamps: true,
+      }
+    );
+//Add the following helpful virtuals to order documents
+orderSchema.virtual('orderTotal').get(function(){
+    return this.lineItems.reduce((total, item) => total + item.extPrice, 0)
+})
 
-  // Simply put, Virtuals are computed properties that are not persisted in the document/database.
-  // By default, a document's virtual properties are not included when the document is serialized, e.g., sent to the client using Express' res.json(personDoc).
+orderSchema.virtual('totalQty').get(function(){
+    return this.lineItems.reduce((total, item) => total + item.qty, 0)
+})
 
-
-  // Add an extPrice to the line item
-lineItemSchema.virtual('extPrice').get(function () {
-    // 'this' is bound to the lineItem subdocument
-    return this.qty * this.item.price;
-  });
-
-  // Yes, we can dot into an embedded subdocument and access its properties as is being done with this.item.price.
-
-
-
-const orderSchema = new Schema({
-    // An order belongs to a user
-    user: { type: Schema.Types.ObjectId, ref: 'User'},
-    // Embed an order's line items is logical
-    lineItems: [lineItemSchema],
-    // A user's unpaid order is their "cart"
-    isPaid: { type: Boolean, default: false},
-
-}, {
-    timestamps: true
+orderSchema.virtual('orderId').get(function(){
+    return this.id.slice(-6).toUpperCase()
 })
 
 
+// statics are callable on the model, not an instance (document)
+orderSchema.statics.getCart = function (userId) {
+  // 'this' is bound to the model (don't use an arrow function)
+  // return the promise that resolves to a cart (the user's unpaid order)
+  return this.findOneAndUpdate(
+    // query
+    { user: userId, isPaid: false },
+    // update - in the case the order (cart) is upserted
+    { user: userId },
+    // upsert option creates the doc if it doesn't exist!
+    { upsert: true, new: true }
+  );
+};
+// Simply put, Virtuals are computed properties that are not persisted in the document/database.
+// By default, a document's virtual properties are not included when the document is serialized, e.g., sent to the client using Express' res.json(personDoc).
+// Add an extPrice to the line item
+lineItemSchema.virtual("extPrice").get(function () {
+    // 'this' is bound to the lineItem subdocument
+    return this.qty * this.item.price;
+});
+
+// Yes, we can dot into an embedded subdocument and access its properties as is being done with this.item.price.
 
 
-
-module.exports = mongoose.model('Order', orderSchema)
+module.exports = mongoose.model("Order", orderSchema);
--- 01-mern-shopping-cart-part-1/starter-code/sei-cafe-codealong/src/pages/NewOrderPage/NewOrderPage.jsx
+++ 01-mern-shopping-cart-part-1/starter-code/sei-cafe-codealong/src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,38 +1,48 @@
-import { useState, useEffect, useRef } from 'react';
-import * as itemsAPI from '../../utilities/items-api';
-import './NewOrderPage.css';
-import { Link } from 'react-router-dom';
-import Logo from '../../components/Logo/Logo';
-import MenuList from '../../components/MenuList/MenuList';
-import CategoryList from '../../components/CategoryList/CategoryList';
-import OrderDetail from '../../components/OrderDetail/OrderDetail';
-import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import { useState, useEffect, useRef } from "react";
+import * as itemsAPI from "../../utilities/items-api";
+// Add the following import
+import * as ordersAPI from '../../utilities/orders-api';
+import "./NewOrderPage.css";
+import { Link } from "react-router-dom";
+import Logo from "../../components/Logo/Logo";
+import MenuList from "../../components/MenuList/MenuList";
+import CategoryList from "../../components/CategoryList/CategoryList";
+import OrderDetail from "../../components/OrderDetail/OrderDetail";
+import UserLogOut from "../../components/UserLogOut/UserLogOut";
+
+
 
 export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
-  const [activeCat, setActiveCat] = useState('');
+  const [activeCat, setActiveCat] = useState("");
   // Obtain a ref object
   const categoriesRef = useRef([]);
+  const [cart, setCart] = useState(null);
 
   // useEffect(function() {
   //   console.log('NewOrderPage rendered');
   // });
-  
-  useEffect(function() {
+
+  useEffect(function () {
     async function getItems() {
       const items = await itemsAPI.getAll();
       categoriesRef.current = items.reduce((cats, item) => {
         const cat = item.category.name;
-        return cats.includes(cat) ? cats : [...cats, cat]
+        return cats.includes(cat) ? cats : [...cats, cat];
       }, []);
       setActiveCat(categoriesRef.current[1]);
       setMenuItems(items);
     }
     getItems();
+
+    // Load cart (a cart is the unpaid order for the logged in user)
+    async function getCart() {
+      const cart = await ordersAPI.getCart();
+      setCart(cart);
+    }
+    getCart();
   }, []);
-  // the empty dependency array above will result in 
-  // the function running after the FIRST render
-  // only
+  // the empty dependency array above will result in the function running after the FIRST render only
 
   return (
     <main className="NewOrderPage">
@@ -43,13 +53,15 @@ export default function NewOrderPage({ user, setUser }) {
           activeCat={activeCat}
           setActiveCat={setActiveCat}
         />
-        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <Link to="/orders" className="button btn-sm">
+          PREVIOUS ORDERS
+        </Link>
         <UserLogOut user={user} setUser={setUser} />
       </aside>
       <MenuList
-        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+        menuItems={menuItems.filter((item) => item.category.name === activeCat)}
       />
-      <OrderDetail />
+      <OrderDetail order={cart}/>
     </main>
   );
 }
